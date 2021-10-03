17337a96c523deae3cba188b21d4750b8b270b9d
Dima <dimalgpf@ukr.net>
2021-10-04 02:48:26 +0300

    4 commands parsing

--- Romanchenko_fi-92_panasuk_fi-94/parser.py
+++ Romanchenko_fi-92_panasuk_fi-94/parser.py
@@ -1,6 +1,4 @@
-from typing import List
 import db, re
-
 
 class Parser:
     def __init__(self):                         #initial action for start of the application
@@ -8,18 +6,18 @@ class Parser:
             query = input('--> ')
             for command in query.split(';'):    #split commands with ';' : CREATE ...; SELECT ... 
                 if command:                     # => in dif. commands
-                    print (self.action(query))
+                    print (self.action(command))
         
     def action(self, query:str) -> str:         #TODO: optimize this shit
         query = self.parse_command(query)       #split input command to the list with needed arguments
-        command = query[0]
-        print (query)
+        command = query[0] if query != -1 else 'error'
+
         if command == 'CREATE':
             _, table_name, columns = query
             action_call = db.create(table_name, columns)
         elif command == 'SELECT':
-            _, table_name = query
-            action_call = db.select(table_name)
+            _, table_name, columns = query
+            action_call = db.select(table_name, columns)
         elif command == 'INSERT':
             _, table_name, values = query
             action_call = db.insert(table_name, values)
@@ -36,20 +34,39 @@ class Parser:
 
     def parse_command(self, query) -> list:     #splits input for list of [command, column, values, *arg]
         str = re.findall(r'\S+', query)
-        for i in str:
-            if i.upper() == 'CREATE' and str.index(i)+1 < len(str):
+        word = str[0]
+        try:
+            if str.index(word)+1 >= len(str): raise Exception('too short')
+            if word.upper() == 'CREATE':
                 columns = {}
-                table_name = str[str.index(i)+1] # if str[0] == CREATE => str[1] = table_name
+                table_name = str[str.index(word)+1] # if str[0] == CREATE => str[1] = table_name
                 m = query.split('(', 1)[1].split(')')[0].replace(',', '').split()   #parse arguments
                 for column in m:
                     if column == 'INDEXED':
                         pass
-                    elif column == m[-1] or m[m.index(column)+1] != 'INDEXED':
+                    elif column == m[-1] or m[m.index(column)+1] != 'INDEXED':   #TODO: regular expression for 'INDEXED'
                         columns[column] = False
                     elif m[m.index(column)+1] == 'INDEXED':
                         columns[column] = True
-                    
+                        
                 return ['CREATE', table_name, columns]
+            elif word.upper() == 'SELECT':
+                selected = str[1]
+                table_name = str[str.index('FROM')+1]       #TODO: regular expresion for 'FROM'
+                
+                return ['SELECT', table_name, selected]
+
+            elif word.upper() == 'INSERT':
+                table_name = str[1]
+                values = query.split('(', 1)[1].split(')')[0].replace(',', '').split()
+                return ['INSERT',table_name, values]
+            elif word.upper() == 'DELETE':
+                table_name = str[str.index('FROM')+1]
+                return ['DELETE', table_name]
+
+        except:
+            print('The list is too short')
+            return [-1]
 
 if __name__ == '__main__':
     client = Parser()
