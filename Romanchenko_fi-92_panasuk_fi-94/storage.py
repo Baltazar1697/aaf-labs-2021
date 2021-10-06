c0091c69f3e972ea355cdbd84c08e77890c5ef20
Dima <dimalgpf@ukr.net>
2021-10-07 01:30:13 +0300

    storage temporary made to be availabe

--- Romanchenko_fi-92_panasuk_fi-94/storage.py
+++ Romanchenko_fi-92_panasuk_fi-94/storage.py
@@ -3,17 +3,17 @@ def create(table_name, columns):
     print ('Table', table_name, 'has been created with columns ', columns)
     return DB
 
+def select(table_name, columns):
+    print ('SELECTED',columns, ' FROM ', table_name)
+    
+def insert(table_name, values):
+    print ('Inserted row with ', values, ' in ', table_name)
+
+def delete(table_name):
+    print ('Deleted ', table_name)
+        
 class database:
     def __init__(self, table_name, columns) -> None:
         self.table_name = table_name
         self.tables = columns
 
-    def select(self, table_name):
-        print ('SELECTED', table_name)
-    
-    def insert(self, table_name, values):
-        print ('Inserted row with ', values, ' in ', table_name)
-
-    def delete(self, table_name):
-        print ('Deleted ', table_name)
-        
